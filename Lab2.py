83073e1669873fff9329904a3671772ed006b69f
EC2 Default User <ec2-user@ip-172-31-52-0.ec2.internal>
2021-01-26 17:00:33 +0000

    my lab 2

---
+++ Lab2.py
@@ -0,0 +1,43 @@
+"""
+Lab 2
+"""
+
+#3.1
+my_name = "Logan"
+
+print(my_name)
+
+#3.2
+
+my_id = 123
+print(my_id)
+
+#3.3
+
+#_123 = my_id
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+#3.4
+my_id_str = '123'
+print(my_id_str)
+
+#3.4
+#print(my_name + my_id)
+
+#3.6
+print(my_name + my_id_str)
+
+#3.7
+
+print(my_name*3)
+
+#3.8
+
+print('hello world. this is my first string.'.split("."))
+
+#3.9
+
+message = "logan's id is 123"
+print(message)
